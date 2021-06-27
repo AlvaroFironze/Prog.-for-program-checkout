c5a6e2d556446856f382794a86b9d56283d4d5fe
MaxMixer <mrmixer1234@yandex.ru>
2021-06-28 00:08:35 +0500

    Второй коммит

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='LogServer',
+    version='0.0.1',
+    author='Stahov Maxim & Milenteva Anastasia',
+    author_email='mdstakhov@edu.hse.ru',
+    packages=['LogServer'],
+    package_dir={'LogServer':""},
+    url='https://github.com/AlvaroFironze/Prog.-for-program-checkout/',
+    description='Program for program checkout',
+
+)
